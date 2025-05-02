8c43f9186bd11e1f94fc42a96bc77ad1807121c3
Mehran Ali <mehran@booknrent.com>
2025-05-02 15:36:10 +0500

    Changes

--- public/script.js
+++ public/script.js
@@ -55,7 +55,7 @@ const SERVER_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : "http://159.223.201.156:3000";
+    : "http://159.223.201.156";
 
 // Show loading state
 const loading = document.getElementById("loading");
